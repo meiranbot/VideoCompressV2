4d92b9ed074762824230e070e1452a4a2ea101f8
meiranbot <87614073+meiranbot@users.noreply.github.com>
2023-11-07 08:48:33 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -85,19 +85,9 @@ async def incoming_start_message_f(bot, update):
         chat_id=update.chat.id,
         text=Localisation.START_TEXT,
         reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton('Updates Channel', url='https://t.me/UniversalBotsUpdate')
-                ],
-                [
-                    InlineKeyboardButton('Support Group', url='https://t.me/UniversalBotsSupport')
-                ]
-            ]
-        ),
-        reply_to_message_id=update.message_id,
     )
-    
-async def incoming_compress_message_f(bot, update):
+    return
+    async def incoming_compress_message_f(bot, update):
   """/compress command"""
   update_channel = UPDATES_CHANNEL
   if update_channel:
